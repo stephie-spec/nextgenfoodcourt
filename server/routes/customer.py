ec17e6a31554bf9035a1be99eb2aa51853c55acc
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2026-01-27 23:31:13 +0300

    Modified Customer Sign Up to validate email and changed return message.

--- server/routes/customer.py
+++ server/routes/customer.py
@@ -1,6 +1,6 @@
 from flask import request
 from flask_restful import Resource
-from server.models import db, Customer, Order, Item, check_password, password_hashed
+from server.models import db, Customer, Order, Item, check_password, hash_password, password_hashed
 # from flask_cors import cross_origin
 
 
@@ -10,16 +10,27 @@ class CustomerSignUp(Resource):
     def post(self):
 
         data = request.get_json()
+
+        if not data.get("name") or not data.get("email") or not data.get("password"):
+            return {"message": "Name, email, and password are required."}, 400
+
+        if Customer.query.filter_by(email=data["email"]).first():
+            return {"message": "Email already exists."}, 400
+
         customer = Customer(
 
-            name=data['name'],
-            email=data['email'],
-            password_hashed=data['password_hashed']
+            name=data["name"],
+            email=data["email"],
+            password_hashed=hash_password(data["password"])
         )
 
         db.session.add(customer)
         db.session.commit()
 
-        return customer.to_dict(), 201
+        return {"message": "Customer created successfully."}, 201
 
     
+class CustomerLogin(Resource):
+
+    pass
+
