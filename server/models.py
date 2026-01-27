3b3522a96fc46b1d67369951b448bc4888a335c2
OgechaEnock <enockogecha7@gmail.com>
2026-01-27 20:11:29 +0300

    add enums for order status and cuisine categories

--- server/models.py
+++ server/models.py
@@ -6,3 +6,17 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import Enum, UniqueConstraint
 
 db = SQLAlchemy()
+class OrderStatus(enum.Enum):
+    pending = "pending"
+    completed = "completed"
+
+
+class CuisineCategory(enum.Enum):
+    kenyan = "kenyan"
+    ethiopian = "ethiopian"
+    nigerian = "nigerian"
+    indian = "indian"
+    chinese = "chinese"
+    italian = "italian"
+    american = "american"
+    other = "other"
