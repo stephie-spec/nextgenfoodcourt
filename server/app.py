a1b2f271a8d713e60693bb03bd9ca87a5342e421
OgechaEnock <enockogecha7@gmail.com>
2026-01-28 21:02:22 +0300

    Add owner routes

---
+++ server/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_restful import Api
+
+from config import Config
+from extensions import db, migrate
+from models import *
+
+from routes.owner import OwnerListResource, OwnerResource
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    api = Api(app)
+
+    api.add_resource(OwnerListResource, "/owners")
+    api.add_resource(OwnerResource, "/owners/<int:owner_id>")
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
