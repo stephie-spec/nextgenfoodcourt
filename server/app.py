3aa1b953e850dc4f173ca01d0ad91fd46e3d81a4
OgechaEnock <enockogecha7@gmail.com>
2026-02-01 09:22:28 +0300

    Remove insecure OwnerResource and OwnerListResource endpoints

--- server/app.py
+++ server/app.py
@@ -4,12 +4,10 @@ from flask_migrate import Migrate
 
 from extensions import db
 from models import *   
-
 from routes.owner import (
-    OwnerListResource,
-    OwnerResource,
     OwnerLoginResource,
-    OwnerAccountResource
+    OwnerSignUp,
+    OwnerDetails
 )
 from routes.order import OrderListResource, OrderResource
 from routes.item import ItemListResource, ItemResource
@@ -32,10 +30,9 @@ def create_app():
     api = Api(app)
 
     # ROUTES
-    api.add_resource(OwnerListResource, "/owners")
-    api.add_resource(OwnerResource, "/owners/<int:owner_id>")
-    api.add_resource(OwnerLoginResource, "/owners/login")
-    api.add_resource(OwnerAccountResource, "/owners/me")
+    api.add_resource(OwnerSignUp, '/api/owner/signup')
+    api.add_resource(OwnerLoginResource, '/api/owner/login')
+    api.add_resource(OwnerDetails, '/api/owner/details')
     api.add_resource(OrderListResource, "/orders")
     api.add_resource(OrderResource, "/orders/<int:order_id>")
     api.add_resource(ItemListResource, "/items")
