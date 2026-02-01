fae6ebc06e38f58a33c3999e8cd2546d4410292c
OgechaEnock <enockogecha7@gmail.com>
2026-02-01 07:41:12 +0300

    set  SECRET_KEY in app configuration

--- server/app.py
+++ server/app.py
@@ -21,6 +21,7 @@ def create_app():
     app = Flask(__name__)
 
     # CONFIG
+    app.config["SECRET_KEY"] = "super-secret-key-change-me"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
