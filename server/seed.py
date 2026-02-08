24e6e176a6e318536ca66d2f2c1c6dc25a5d2a07
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2026-02-08 16:15:58 +0300

    Fixed seed error. Added all seeded items to all_items[] to allow e aiser indexing.

--- server/seed.py
+++ server/seed.py
@@ -203,6 +203,9 @@ if __name__ == "__main__":
         db.session.add_all(additional_items)
         db.session.commit()
 
+        # Adding new items to the existing all_items list for easier reference.
+        all_items.extend(additional_items)
+
         # Link items to outlets
         print("Creating menu outlet items...")
         menu_items = []
@@ -457,18 +460,19 @@ if __name__ == "__main__":
         print("Creating customer favourites...")
         
         # Add some items to customer1's favourites
-        customer1.favourite_items.append(CustomerFavourite(item_id=all_items[39].id))  # Shawarma
-        customer1.favourite_items.append(CustomerFavourite(item_id=all_items[5].id))  # Suya Skewers
-        customer1.favourite_items.append(CustomerFavourite(item_id=all_items[10].id))  # Pilau Rice
+        customer1.favourites.append(CustomerFavourite(item_id=all_items[38].id))  # Shawarma
+        customer1.favourites.append(CustomerFavourite(item_id=all_items[4].id))  # Jollof Rice
+        customer1.favourites.append(CustomerFavourite(item_id=all_items[8].id))  # Pilau Rice
         
         # Add some items to customer2's favourites
-        customer2.favourite_items.append(CustomerFavourite(item_id=all_items[39].id))  # Shawarma
-        customer2.favourite_items.append(CustomerFavourite(item_id=all_items[5].id))  # Suya Skewers
-        customer2.favourite_items.append(CustomerFavourite(item_id=all_items[15].id))  # Nyama Choma
+        customer2.favourites.append(CustomerFavourite(item_id=all_items[38].id))  # Shawarma
+        customer2.favourites.append(CustomerFavourite(item_id=all_items[5].id))  # Suya Skewers
+        customer2.favourites.append(CustomerFavourite(item_id=all_items[13].id))  # Nyama Choma
         
         # Add some items to customer3's favourites
-        customer3.favourite_items.append(CustomerFavourite(item_id=all_items[2].id))  # Kitfo
-        customer3.favourite_items.append(CustomerFavourite(item_id=all_items[39].id))  # Shawarma
-        customer3.favourite_items.append(CustomerFavourite(item_id=all_items[15].id))  # Nyama Choma
+        customer3.favourites.append(CustomerFavourite(item_id=all_items[2].id))  # Kitfo
+        customer3.favourites.append(CustomerFavourite(item_id=all_items[38].id))  # Shawarma
+        customer3.favourites.append(CustomerFavourite(item_id=all_items[13].id))  # Nyama Choma
         
         db.session.commit()
+
