b8dbc466051b10b71c9677dda8d3de4e027a387a
OgechaEnock <enockogecha7@gmail.com>
2026-02-08 09:11:01 +0300

    adds /api prefix

--- server/app.py
+++ server/app.py
@@ -60,7 +60,7 @@ def create_app():
     api.add_resource(CustomerDetails, "/api/customer/details")
     api.add_resource(CustomerSignUp, "/api/customer/signup")
     api.add_resource(ListOutlets, "/api/outlets")
-    api.add_resource(OutletResource, "/outlets/<int:outlet_id>")
+    api.add_resource(OutletResource, "/api/outlets/<int:outlet_id>")
     api.add_resource(OutletMenu, "/api/outlet/<int:outlet_id>/menu")
     api.add_resource(TestimonialListResource, '/api/testimonials')
     api.add_resource(TestimonialResource, '/api/testimonials/<string:testimonial_id>')
