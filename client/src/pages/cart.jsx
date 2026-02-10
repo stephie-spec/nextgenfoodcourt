b351cad3a256e19cf7465c26e641f12ac9da8772
billotiende-droid <bill.otiende@student.moringaschool.com>
2026-02-10 12:48:46 +0300

    Import useEffect

--- client/src/pages/cart.jsx
+++ client/src/pages/cart.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ShoppingCart, Trash2, Plus, Minus, ArrowLeft, ArrowRight, Store, Clock, MapPin, Phone, CreditCard, Shield, Truck, Star, Info, Tag, X } from 'lucide-react';
