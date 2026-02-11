4c86fda7c9e42f6c50487c2c3cd2efca6a7f9f2f
billotiende-droid <bill.otiende@student.moringaschool.com>
2026-02-11 23:23:52 +0300

    add customer favorites logic and top-trending items endpoint

--- server/routes/favourite.py
+++ server/routes/favourite.py
@@ -96,26 +96,33 @@ class TopFavourites(Resource):
             .all()
         )
 
-        results = []
+        customer = require_customer()
+        favorite_ids = set()
+        if customer:
+            favorite_ids = {
+                fav.item_id
+                for fav in CustomerFavourite.query.filter_by(customer_id=customer.id).all()
+            }
 
-        for item, favourite_count in top_items :
-            outlet = None
+        response = []
+        for item, favourite_count in top_items:
+            menu_link = MenuOutletItem.query.filter_by(item_id=item.id).first()
+            outlet = Outlet.query.get(menu_link.outlet_id) if menu_link else None
 
-            if item.menu_links :
-                outlet =  item.menu_links[0].outlet # To return the linked outlet for each item, if it is found.
-            
-            results.extend ([
+            response.append(
                 {
-                    "id" : item.id,
-                    "name" : item.name,
-                    "price" : item.price,
-                    "image" : item.image,
-                    "outlet_id" : outlet.id if outlet else None,
-                    "outlet_name" : outlet.name if outlet else None,
-                    "description" : item.description,
-                    "category_name" : item.category_name,
-                    "favourite_count" : favourite_count
+                    "id": item.id,
+                    "name": item.name,
+                    "price": item.price,
+                    "image": item.image,
+                    "image_path": item.image,
+                    "favourite_count": favourite_count,
+                    "is_favorite": item.id in favorite_ids,
+                    "outlet_id": menu_link.outlet_id if menu_link else None,
+                    "outlet_name": outlet.name if outlet else None
                 }
-            ])
-        
-        return results, 200
+            )
+
+        return response, 200
+
+    
